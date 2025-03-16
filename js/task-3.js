b627e914e47a2cd138c49d670708302dca122206
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-03-16 10:06:27 +0000

    create

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,16 @@
-function getElementWidth(content, padding, border) {
-  content = parseFloat(content);
-  padding = parseFloat(padding);
-  border = parseFloat(border);
-  const getElementWidth = content + padding * 2 + border * 2;
-  return getElementWidth;
+function checkForSpam(message) {
+  const hasSpam = message.toLowerCase();
+
+  if (hasSpam.includes('sale') || hasSpam.includes('spam')) {
+    return true;
+  }
+  return false;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
